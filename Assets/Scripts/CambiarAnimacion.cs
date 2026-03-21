6016a13dab4b8bcbe26f589e2a5ba40d47598334
a01754495 <a01754495@tec.mx>
2026-03-21 01:16:56 -0600

    Añadidas animaciones

---
+++ Assets/Scripts/CambiarAnimacion.cs
@@ -0,0 +1,38 @@
+using System;
+using UnityEngine;
+
+public class CambiarAnimacion : MonoBehaviour
+{
+    private Animator animator;
+    private Rigidbody2D rb;
+    private SpriteRenderer sr;
+    private EstadoPersonaje estado;
+    private MoverPersonaje mover;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
+        sr = GetComponent<SpriteRenderer>();
+        estado = GetComponentInChildren<EstadoPersonaje>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //animator.SetFloat("velocidad", MathF.Abs(rb.linearVelocityX));
+        animator.SetFloat("velocidad", Math.Abs(rb.linearVelocityX));
+        //animator.SetFloat("velocidadAbs", Math.Abs(rb.linearVelocityX));
+
+        //print("Actualizando velocidad: ");
+        //print((float)(rb.linearVelocityX));
+
+        // Manejar el FLIP_x
+        sr.flipX = (sr.flipX || rb.linearVelocityX < -0.1) && rb.linearVelocityX <= 0;
+        
+
+        // Manejar animación de salto
+        animator.SetBool("enPiso", estado.estaEnPiso);
+    }
+}
