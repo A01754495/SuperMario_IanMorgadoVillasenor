9442585677ae23497b8a33bfc319300e037d8f27
a01754495 <a01754495@tec.mx>
2026-03-21 23:05:34 -0600

    Añadidos créditos

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -8,16 +8,22 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UIElements;
 using System;
+//using TextElement = UnityEngine.UIElements.TextElement;
 
 public class Menu : MonoBehaviour
 {
     private UIDocument menu;
+    private VisualElement titulo;
     private VisualElement botones;
+    private VisualElement autor;
     private VisualElement menuAyuda;
+    private VisualElement menuCreditos;
     private Button botonJugar;
     private Button botonAyuda;
     private Button botonCreditos;
-    private Button volverBotones;
+    private Button volverBotones;   
+    private Button salirCreditos;
+    private Label creditos;
 
     void OnEnable()
     {
@@ -28,14 +34,21 @@ public class Menu : MonoBehaviour
         botonAyuda = root.Q<Button>("BotonAyuda");
         botonCreditos = root.Q<Button>("BotonCreditos");
         volverBotones = root.Q<Button>("VolverBotones");
+        salirCreditos = root.Q<Button>("SalirCreditos");
 
+        creditos = root.Q<Label>("Creditos");
+
+        titulo = root.Q<VisualElement>("Titulo");
         botones = root.Q<VisualElement>("Botones");
+        autor = root.Q<VisualElement>("Autor");
         menuAyuda = root.Q<VisualElement>("MenuAyuda");
+        menuCreditos = root.Q<VisualElement>("MenuCreditos");
 
         botonJugar.clicked += AbrirJugar;
         botonAyuda.clicked += AbrirAyuda;
         botonCreditos.clicked += AbrirCreditos;
         volverBotones.clicked += VolverBotones;
+        salirCreditos.clicked += VolverMenu;
     }
 
     void OnDisable()
@@ -44,27 +57,44 @@ public class Menu : MonoBehaviour
         botonAyuda.clicked -= AbrirAyuda;
         botonCreditos.clicked -= AbrirCreditos;
         volverBotones.clicked -= VolverBotones;
+        salirCreditos.clicked -= VolverMenu;
     }
 
+    // Carga mapa
     private void AbrirJugar()
     {
         SceneManager.LoadScene("PrimerPantalla");
     }
 
+    // Desactiva Display de los botones y activa el de la Ayuda
     private void AbrirAyuda()
     {
         botones.style.display = DisplayStyle.None;
         menuAyuda.style.display = DisplayStyle.Flex;
     }
 
+    // Desactiva todos los VisualElements y activa los créditos
     private void AbrirCreditos()
     {
-        //SceneManager.LoadScene("PrimerPantalla");
+        titulo.style.display = DisplayStyle.None;
+        botones.style.display = DisplayStyle.None;
+        autor.style.display = DisplayStyle.None;
+        menuCreditos.style.display = DisplayStyle.Flex;
     }
 
+    // Desactiva Display de Ayuda y activa el de los botones
     private void VolverBotones()
     {
         menuAyuda.style.display = DisplayStyle.None;
         botones.style.display = DisplayStyle.Flex;
+    }
+
+    // Desactiva los créditos y reactiva los VisualElements del menu
+    private void VolverMenu()
+    {
+        menuCreditos.style.display = DisplayStyle.None;
+        titulo.style.display = DisplayStyle.Flex;
+        botones.style.display = DisplayStyle.Flex;
+        autor.style.display = DisplayStyle.Flex;
     }
 }
